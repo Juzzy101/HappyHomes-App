9ab16c343779212b15e58d0a6ddc3e0810ac95e4
Juzzy101 <justynazycka@gmail.com>
2025-05-20 19:13:29 +0100

    Added The Property Class to be used to create and get details of the property in the app

---
+++ Properties.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyHomes_App
+{
+    public class Properties
+    {
+        public string PropertyID { get; set; }
+        public string PropertyType { get; set; }
+        public string Address { get; set; }
+        //Constructor to create any Property Objects
+        public Properties(string propertyID, string propertyType, string address)
+        {
+            PropertyID = propertyID;
+            PropertyType = propertyType;
+            Address = address;
+        }
+        //Method to get details of Property
+        public string GetDetails()
+        {
+            return PropertyType + " - " + Address + " - " + PropertyID;
+        }
+
+    }
+}
