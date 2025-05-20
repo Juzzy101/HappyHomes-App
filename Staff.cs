a25d564d384649944e12efc30745b88896b68857
Juzzy101 <justynazycka@gmail.com>
2025-05-20 20:07:18 +0100

    added viewings and staff classess with base details and constructors

---
+++ Staff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyHomes_App
+{
+    public class Staff
+    {
+        public string Name { get; set; }
+        public List <DateTime> UnavailableTimes { get; set; }
+
+        public Staff(string name)
+        {
+            Name = name;
+            //create a list for that staff member
+            UnavailableTimes = new List<DateTime>();
+        }
+        //Method to check if staff member is available at a specific time
+        public bool IsAvailable(DateTime time) 
+        {
+            return !UnavailableTimes.Contains(time);
+        }
+    }
+}
---
+++ Viewings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyHomes_App
+{
+    public class Viewings
+    {
+        public Customers Customers { get; set; }
+        public Properties Properties { get; set; }
+
+        public Staff Staff { get; set; }
+        public DateTime ViewingTime { get; set; }
+        public string Status { get; set; }
+
+        public Viewings(Customers customers, Properties properties, Staff staff, DateTime viewingTime)
+        {
+            Customers = customers;
+            Properties = properties;
+            Staff = staff;
+            ViewingTime = viewingTime;
+            Status = "Bookings";
+        }
+    }
+}
