addcc6b8aef37004f3869a9bb74f33a18191dfd9
Juzzy101 <justynazycka@gmail.com>
2025-05-25 12:17:29 +0100

    comments changed

--- Staff.cs
+++ Staff.cs
@@ -9,7 +9,7 @@ namespace HappyHomes_App
     public class Staff
     {
         public string Name { get; set; }
-        public List <DateTime> UnavailableTimes { get; set; }
+        public List<DateTime> UnavailableTimes { get; set; }
 
         public Staff(string name)
         {
@@ -18,7 +18,7 @@ namespace HappyHomes_App
             UnavailableTimes = new List<DateTime>();
         }
         //Method to check if staff member is available at a specific time
-        public bool IsAvailable(DateTime time) 
+        public bool IsAvailable(DateTime time)
         {
             return !UnavailableTimes.Contains(time);
         }
